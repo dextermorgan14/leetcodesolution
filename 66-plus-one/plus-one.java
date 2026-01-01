db17533f36f971a4732f017f412b8ae5d2357de0
ShaShank Shandilya <96918351+dextermorgan14@users.noreply.github.com>
2026-01-01 22:52:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (19.41%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+          int n = digits.length;
+
+        int i = n - 1; // start from LSB like normal addition
+
+        while (i >= 0) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            // digit was 9
+            digits[i] = 0;
+            i--;
+        }
+
+        // carry reached beyond the most significant digit
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
+    }
+}
